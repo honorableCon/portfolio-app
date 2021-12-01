a9b3fae52a8267c0293b6fe8cb406507c7ee6784
Honorable con <honorablecon@gmail.com>
2021-12-01 12:36:54 +0000

    SideBar Navigation Integrated

--- app/src/main/java/com/honorablecon/portfolio/MainActivity.java
+++ app/src/main/java/com/honorablecon/portfolio/MainActivity.java
@@ -1,13 +1,60 @@
 package com.honorablecon.portfolio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView sidebar_nav_code;
+    private TextView sidebar_nav_scolary;
+    private TextView sidebar_nav_realized;
+    private TextView sidebar_nav_hobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sidebar_nav_code = findViewById(R.id.sidebar_nav_code);
+        sidebar_nav_scolary = findViewById(R.id.sidebar_nav_scolary);
+        sidebar_nav_realized = findViewById(R.id.sidebar_nav_realized);
+        sidebar_nav_hobbies = findViewById(R.id.sidebar_nav_hobbies);
+
+        sidebar_nav_code.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+        sidebar_nav_scolary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MyEducation.class));
+            }
+        });
+        /*sidebar_nav_code.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+        sidebar_nav_code.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+        sidebar_nav_code.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MyServices.class));
+            }
+        });*/
     }
 }
---
+++ app/src/main/java/com/honorablecon/portfolio/SideNavigator.java
@@ -0,0 +1,19 @@
+package com.honorablecon.portfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SideNavigator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_side_navigator);
+
+    }
+}
